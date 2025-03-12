bd2db38498d12b50a608a9c047362c1d6743de9c
AlekseyK <alekkalm@gmail.com>
2025-03-12 16:53:40 +0300

    103_tanks сделал поворот башни кнопками
    файлы svg больше не используются.
    svg прописаны в файле index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 //получаем первый танк
-const t1shassi = fetch('svg/t1-shassi.SVG')
-    .then(response => response.text())
-    .then(svg => {
-      document.getElementById('ant-template').innerHTML = svg;
-    });
-
-
-
+const svg_t1k = document.getElementById('t1k'); 
+const svg_t1b = document.getElementById('t1b');
+svg_t1k.style.transformOrigin = 'center center';
+svg_t1b.style.transformOrigin = '0 0';
+const t1 = {
+    svg_t1k: svg_t1k,
+	svg_t1b: svg_t1b,
+    x: 0,
+    y: 0,
+    angle: 0,
+    speed: 1,
+  };
 
 
 
@@ -51,6 +55,44 @@ document.addEventListener('mousemove', (event) => {
 document.addEventListener('mouseout', () => {
   mousex = -1000;
   mousey = -1000;
+});
+
+//нажатые клавиши
+let keyA_pressed = false;
+let keyW_pressed = false;
+let keyS_pressed = false;
+let keyD_pressed = false;
+let ArrowUp_pressed = false;
+let ArrowDown_pressed = false;
+let ArrowLeft_pressed = false;
+let ArrowRight_pressed = false;
+
+// Обработчик события нажатия клавиши
+document.addEventListener('keydown', (event) => {
+  switch (event.code) {
+	case 'KeyA': keyA_pressed = true; break; 
+    case 'KeyW': keyW_pressed = true; break; 
+    case 'KeyS': keyS_pressed = true; break;
+    case 'KeyD': keyD_pressed = true; break;
+	case 'ArrowUp': ArrowUp_pressed = true; break; 
+    case 'ArrowDown': ArrowDown_pressed = true; break; 
+    case 'ArrowLeft': ArrowLeft_pressed = true; break;
+    case 'ArrowRight': ArrowRight_pressed = true; break;
+  }
+});
+
+// Обработчик события отпускания клавиши
+document.addEventListener('keyup', (event) => {
+  switch (event.code) {
+	case 'KeyA': keyA_pressed = false; break; 
+    case 'KeyW': keyW_pressed = false; break; 
+    case 'KeyS': keyS_pressed = false; break;
+    case 'KeyD': keyD_pressed = false; break;
+	case 'ArrowUp': ArrowUp_pressed = false; break; 
+    case 'ArrowDown': ArrowDown_pressed = false; break; 
+    case 'ArrowLeft': ArrowLeft_pressed = false; break;
+    case 'ArrowRight': ArrowRight_pressed = false; break;
+  }
 });
 
 function updateAnts() {
@@ -102,6 +144,21 @@ function updateAnts() {
 
     let angle = ant.angle + 90; // Корректировка угла поворота
     ant.element.style.transform = `rotate(${angle}deg)`;
+	
+	
+	if(keyA_pressed) t1.x -= 0.1;
+	if(keyD_pressed) t1.x += 0.1;
+	if(keyW_pressed) t1.y -= 0.1;
+	if(keyS_pressed) t1.y += 0.1;
+	if(ArrowLeft_pressed) t1.angle -= 0.1;
+	if(ArrowRight_pressed) t1.angle += 0.1;
+	
+	//t1.x += 1;
+    //t1.y += 1;
+	//t1.angle += 1;
+	t1.svg_t1k.style.transform = `translate(${t1.x}px, ${t1.y}px)rotate(${t1.angle}deg)`;
+	//                                                           |смещение башни по y| |центр вращения башни 35px, 15px|
+	t1.svg_t1b.style.transform = `translate(${t1.x}px, ${t1.y}px) translate(0px, 10px) translate(35px, 15px) rotate(${t1.angle}deg) translate(-35px, -15px)`;
   });
 
   requestAnimationFrame(updateAnts);
